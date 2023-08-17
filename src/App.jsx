ecb17d0d340fe5d5f504370ea32b871849ef71f0
chandubomma <bommachandugoud@gmail.com>
2023-08-17 10:36:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex flex-col justify-center items-center'>
+        <h2 className='text-amber-500 text-4xl font-bold'>Hello Hungrezy</h2>
+    </div>
+  )
+}
+
+export default App
