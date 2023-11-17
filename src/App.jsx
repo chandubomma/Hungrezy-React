791e26ac131f2c816e364e4b289d2c7658d43aee
Sabbini-susritha <139756855+Sabbini-susritha@users.noreply.github.com>
2023-11-17 10:36:02 +0530

    Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import SignUpForm from "./pages/SignUpForm"
-import SigninForm from "./pages/SigninForm"
-import UserDetailsForm from "./components/forms/UserDetailsForm"
+import SignUpForm from "./pages/SignUpForm";
+import SigninForm from "./pages/SigninForm";
+import {
+  Outlet,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+
+const Root = () => {
+  return (
+    <React.Fragment>
+      <Outlet />
+    </React.Fragment>
+  );
+};
+
+const Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+      <Route path="/signup" element={<SignUpForm />} />
+      <Route path="/signin" element={<SigninForm />} />
+    </Route>
+  )
+);
 
 const App = () => {
   return (
-    <div className='w-screen h-screen flex flex-col justify-center items-center'>
-        <SignUpForm/>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <RouterProvider router={Router} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
