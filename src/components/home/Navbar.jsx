950c05ac88b980b7e382e05cc018e3de443391d4
Sabbini-susritha <139756855+Sabbini-susritha@users.noreply.github.com>
2023-11-18 17:32:50 +0530

    Animation

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link, useLocation } from "react-router-dom";
 import logo from "./../../assets/logoAsset.png";
 import { FaUser, FaBars } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
+import { motion } from "framer-motion";
+
 const Navbar = () => {
   const { pathname } = useLocation();
   const [showDropdown, setShowDropdown] = useState(false);
@@ -29,7 +31,12 @@ const Navbar = () => {
 
   return (
     <div className="w-full">
-      <div className="hidden lg:flex px-48 py-10 w-full justify-between items-center">
+      <motion.div
+        className="hidden lg:flex px-48 py-10 w-full justify-between items-center"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
         <Link
           to="/"
           className="flex align-middle items-center gap-2 transition-transform transform hover:scale-110 hover:opacity-80"
@@ -75,15 +82,25 @@ const Navbar = () => {
             Contact Us
           </Link>
         </div>
-        <div className="relative">
-          <div
+        <motion.div
+          className="relative"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <motion.div
             className="w-fit h-fit border-[1.5px] p-2 rounded-full border-gray-500 hover:scale-110 transition-transform transform cursor-pointer"
             onClick={handleUserClick}
           >
             <FaUser className="text-2xl text-gray-500" />
-          </div>
+          </motion.div>
           {showDropdown && (
-            <div className="absolute top-12 right-0 bg-white border border-gray-400 p-2 w-36 rounded shadow-md flex flex-col items-center">
+            <motion.div
+              className="absolute top-12 right-0 bg-white border border-gray-400 p-2 w-36 rounded shadow-md flex flex-col items-center"
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.2 }}
+            >
               <Link
                 to="/signup"
                 className="block py-1 px-4 hover:bg-gray-50 hover:font-semibold text-gray-800 border-b border-gray-300 w-full text-center"
@@ -96,18 +113,24 @@ const Navbar = () => {
               >
                 Sign In
               </Link>
-              <button
+              <motion.button
                 onClick={handleLogout}
                 className="block py-1 px-4 hover:font-semibold hover:bg-gray-50 text-gray-800 border-gray-300 w-full text-center"
+                whileHover={{ scale: 1.05 }}
               >
                 Logout
-              </button>
-            </div>
+              </motion.button>
+            </motion.div>
           )}
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
 
-      <div className="lg:hidden px-10 py-10 flex justify-between items-center">
+      <motion.div
+        className="lg:hidden px-10 py-10 flex justify-between items-center"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
         <Link
           to="/"
           className="flex align-middle items-center gap-2 transition-transform transform hover:scale-110 hover:opacity-80"
@@ -115,15 +138,25 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-16" />
           <h2 className="font-bold text-2xl">Hungrezy</h2>
         </Link>
-        <div className="relative">
-          <div
+        <motion.div
+          className="relative"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <motion.div
             className="w-fit h-fit border-[1.5px] p-2 rounded-full border-gray-500 hover:scale-110 transition-transform transform cursor-pointer"
             onClick={handleUserClick}
           >
             <FaUser className="text-2xl text-gray-500" />
-          </div>
+          </motion.div>
           {showDropdown && (
-            <div className="absolute top-12 right-0 bg-white border border-gray-300 p-2 w-32 rounded shadow-md">
+            <motion.div
+              className="absolute top-12 right-0 bg-white border border-gray-300 p-2 w-32 rounded shadow-md"
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
               <Link
                 to="/signup"
                 className="block py-1 px-4 hover:font-semibold text-gray-800 hover:text-amber-500 hover:scale-105 transition-colors duration-300 rounded border-b border-gray-300"
@@ -136,34 +169,50 @@ const Navbar = () => {
               >
                 SignIn
               </Link>
-              <button
+              <motion.button
                 onClick={handleLogout}
                 className="block py-1 px-4 hover:font-semibold text-gray-800 hover:text-red-500 hover:scale-105 transition-colors duration-300 rounded border-b border-gray-300"
+                whileHover={{ scale: 1.05 }}
               >
                 Logout
-              </button>
-            </div>
+              </motion.button>
+            </motion.div>
           )}
-        </div>
+        </motion.div>
         <div>
           <FaBars
             className="text-3xl cursor-pointer"
             onClick={handleDrawerToggle}
           />
         </div>
-      </div>
+      </motion.div>
 
       {/* Drawer for Mobile */}
       {isDrawerOpen && (
-        <div className="lg:hidden fixed inset-0 bg-black/80 bg-opacity-30 z-50">
-          <div className="bg-white h-full w-72 p-4 fixed top-0 left-0">
+        <motion.div
+          className="lg:hidden fixed inset-0 bg-black/80 bg-opacity-30 z-50"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <motion.div
+            className="bg-white h-full w-72 p-4 fixed top-0 left-0"
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
             <div className="flex justify-end">
               <FaXmark
                 className="text-3xl cursor-pointer"
                 onClick={handleDrawerToggle}
               />
             </div>
-            <div className="flex flex-col gap-4">
+            <motion.div
+              className="flex flex-col gap-4"
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
               <Link
                 to="/"
                 className={`${
@@ -202,9 +251,9 @@ const Navbar = () => {
               >
                 Contact Us
               </Link>
-            </div>
-          </div>
-        </div>
+            </motion.div>
+          </motion.div>
+        </motion.div>
       )}
     </div>
   );
