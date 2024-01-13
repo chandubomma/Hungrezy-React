170e61fe8df0bb548830786e1cc0407841556f1a
chandubomma <chandu@examtrakker.com>
2024-01-13 16:10:03 +0530

    added - EmailAndPassword form component.

---
+++ src/components/forms/EmailAndPassword.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react"
+
+const EmailAndPassword = ({setSignInWithOTP,email,handleEmail}) => {
+    const [password,setPassword] = useState("");
+
+    const handleSignIn = ()=>{
+        
+    }
+
+    const handlePasswordChange = (e)=>{
+        setPassword(e.target.value)
+      }
+
+  return (
+    <div className="w-80 ">
+        <Heading/>
+        <div className="form-floating mb-3 mt-3">
+          <input 
+            className="form-control focus:shadow-none focus:border-amber-600 rounded-sm" 
+            id="email"
+            placeholder="Enter Email"
+            name="email"
+            type="email"
+            value={email}
+            onChange={(e)=>handleEmail(e)}
+            autoFocus
+          />
+          <label 
+            htmlFor="email"
+            className="text-gray-500"
+          >
+            Email
+          </label>
+        </div>
+
+        <div className="form-floating mt-3">
+          <input
+            className="form-control focus:shadow-none focus:border-amber-600 rounded-sm" 
+            id="password" 
+            placeholder="Enter Password" 
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e)=>handlePasswordChange(e)} 
+          />
+          <label 
+            htmlFor="password"
+            className="text-gray-500"
+          >
+            Password
+          </label>
+        </div>
+        <div className="flex justify-end mt-1">
+          <h6 className="text-gray-500 text-sm w-fit hover:cursor-pointer hover:text-gray-400" onClick={()=>setSignInWithOTP(true)}>
+            sign in with OTP?
+          </h6>
+        </div>
+        <div className="mt-6">
+          <button onClick={handleSignIn} className="h-10 w-full bg-amber-500 text-white text-md font-semibold hover:bg-amber-600 rounded-none">
+            Sign in
+          </button>
+        </div>
+        <div className="mt-16 flex flex-row ">
+          <h6 className="text-gray-500 font-medium text-sm w-fit hover:cursor-pointer hover:text-gray-400">
+            New to Hungrezy? Register Here
+          </h6>
+        </div>
+    </div>
+  )
+}
+
+export default EmailAndPassword
+
+
+const Heading = ()=>{
+
+    return(
+      <div className="mb-8">
+        <h1 className="text-4xl font-bold mb-3"><span className="text-green-800">Hun</span><span className="text-amber-500">Grezy</span></h1>
+        <h4 className="text-lg font-medium text-gray-500">Welcome back!</h4>
+      </div>
+    )
+  }
+  
