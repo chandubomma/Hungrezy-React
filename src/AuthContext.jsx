6a75315cf99f2e5cbb2b4eeba9edaa7268766409
= <=>
2024-02-19 18:43:19 +0530

    feat - AuthProvider.

---
+++ src/AuthContext.jsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    const storedAccessToken = localStorage.getItem('accessToken');
+    if (storedAccessToken) {
+      setAccessToken(storedAccessToken);
+    }
+  }, []);
+
+  const signin = async (accessToken,user) => {
+      setAccessToken(accessToken);
+      setUser(user);
+      localStorage.setItem('accessToken', accessToken);
+  };
+
+  const signout = () => {
+    setUser(null);
+    setAccessToken(null);
+    localStorage.removeItem('accessToken');
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, accessToken, signin, signout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { AuthProvider } from './AuthContext';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider>
     <Provider store={store}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Provider>
   </ChakraProvider>
 );
